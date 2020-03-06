872816ffd3b1fb93b37961e261adc51b528cf722
Ranjeet Kumar <ranjeet.kumar@be-mobile.com>
2020-03-06 11:27:35 +0200

    Updates

---
+++ precache-manifest.aa0a332f581f57fb8a3d80cfc1f0782c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f06c93757f60037449446fac69ee2e62",
+    "url": "/contactbook/index.html"
+  },
+  {
+    "revision": "e03c1d351715e187ee6f",
+    "url": "/contactbook/static/css/main.52df4655.chunk.css"
+  },
+  {
+    "revision": "5de1ce1081220b6c1c21",
+    "url": "/contactbook/static/js/2.579974a8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/contactbook/static/js/2.579974a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e03c1d351715e187ee6f",
+    "url": "/contactbook/static/js/main.f4867b76.chunk.js"
+  },
+  {
+    "revision": "3e2bb42c7026bd8545b5",
+    "url": "/contactbook/static/js/runtime-main.2d2a2f66.js"
+  }
+]);
